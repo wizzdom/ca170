ba41e4d2d156b27ceea6b5f812e47e1d0d5c6463
wizzdom <dom@wizzdom.xyz>
2023-02-22 09:42:52 +0000

    week-06 lab exam

---
+++ week-06/grep2.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# grep "queen" /users/tutors/mhumphrysdculab/share/shakespeare/macbeth/complete.html
+grep "queen" macbeth.htm
---
+++ week-06/grep3.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+grep -i "queen" macbeth.htm
